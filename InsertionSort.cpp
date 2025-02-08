ded7e03d037e92516ac79e36e8455e3cf5a3fe04
Yasin Arafat Shibloo <yasinarafat@Yasins-MacBook-Air.local>
2025-02-08 20:53:30 +0600

    Added code

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,17 +1,16 @@
+// --------insertionSort-------//
 #include <iostream>
 using namespace std;
 
 void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {  // Start from the second element
-        int key = arr[i];          // Pick the element to be placed correctly
+    for (int i = 1; i < n; i++) {  
+        int key = arr[i];
         int j = i - 1;
-
-        // Move elements of arr[0..i-1] greater than key to one position ahead
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
-        arr[j + 1] = key; // Insert key at its correct position
+        arr[j + 1] = key;
     }
 }
 

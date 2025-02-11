811b3345aaf67e6545dc2f1fb3fb9bc562c3e6ff
yasin-arafat02 <arn.shibloo02@gmail.com>
2025-02-11 14:03:09 +0600

    Update

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -5,7 +5,7 @@ void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex])
+            if (arr[j] < arr[i])
                 minIndex = j;
         }
         swap(arr[i], arr[minIndex]);

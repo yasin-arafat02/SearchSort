3e57b85633ee15ca77337a8ac12011f338fc0bb0
yasin-arafat02 <arn.shibloo02@gmail.com>
2025-02-14 01:18:15 +0600

    edit

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -4,9 +4,15 @@ void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[i])
+            if (arr[j] < arr[i]){
                 minIndex = j;
-        }
+                for (int k = 0; k < n; k++) {
+                    cout << arr[k] << " ";
+                }
+                cout<<endl;
+            }
+            }
+        
         swap(arr[i], arr[minIndex]);
     }
 }
